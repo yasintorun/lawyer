5e58ab073bb11472a0bcae395c091fe192538cf9
Yasin Torun <yasintorun4680@gmail.com>
2023-05-29 20:35:50 +0300

    Added email and fixed mobile responsive

--- src/pages/messages.js
+++ src/pages/messages.js
@@ -2,12 +2,11 @@ import { getMessages } from '@/lib/queries/message'
 import React from 'react'
 import moment from 'moment';
 import Link from 'next/link';
-moment.locale("tr"); 
 const messages = ({ messages }) => {
     return (
         <div>
             <div className="flex justify-center">
-                <div className="w-1/2 my-5">
+                <div className="md:w-1/2 p-2 my-5">
                     <Link href="/" className='text-sm underline'>
                         &#8592;
                         Anasayfaya Dön
@@ -17,7 +16,10 @@ const messages = ({ messages }) => {
                         {messages.map((message, index) => (
                             <div key={index} className="border border-gray-200 rounded-md p-4 mb-4">
                                 <div className="flex justify-between">
-                                    <div className="font-bold">{message.name}</div>
+                                    <div>
+                                        <div className="font-bold">{message.name}</div>
+                                        <a href={'mailto:'+message.email} className='text-sm text-slate-500'>{message.email}</a>
+                                    </div>
                                     <div className="text-sm">{moment(message.created_date).format('LLL')}</div>
                                 </div>
                                 <div className="mt-2">{message.message}</div>
